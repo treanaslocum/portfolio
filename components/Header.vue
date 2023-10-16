9ac4e7ba926ff6d742f86e495dd570fbe0b1c3bb
Treana Slocum <treanaslocum.com>
2023-10-16 14:29:36 -0700

    Changed Linkedin link url

--- components/Header.vue
+++ components/Header.vue
@@ -9,7 +9,7 @@
           <a href="https://docs.google.com/document/d/18V9ysqe2EyJK23ipfWqxVZH_BqNakcqhbOgt63ZP8KE/edit?usp=sharing" target="_blank" class="mr-8 font-medium">Resume</a>
           <nuxt-link to="/#portfolio" class="lg:mr-16 sm:mr-2 font-medium">Portfolio</nuxt-link>
           <div class="flex">
-            <a href="https://www.linkedin.com/in/treana-slocum-2257a01bb/" target="_blank"><img class="mr-4 h-5 w-5" src="~assets/images/linkedin.png" alt="Linkedin"></a>
+            <a href="https://www.linkedin.com/in/treanaslocum" target="_blank"><img class="mr-4 h-5 w-5" src="~assets/images/linkedin.png" alt="Linkedin"></a>
             <a href="https://github.com/treanaslocum" target="_blank"><img class="mr-4 h-5 w-5" src="~assets/images/github.png" alt="Github"></a>
             <a href="https://dribbble.com/treanaslocum" target="_blank"><img class="mr-4 h-5 w-5" src="~assets/images/dribbble.png" alt="Dribbble"></a>
             <a href="https://www.youtube.com/channel/UC1SWy-nzQvtInvVa0gygw-A" target="_blank"><img class="h-4 w-6" src="~assets/images/youtube.png" alt="Youtube"></a>
